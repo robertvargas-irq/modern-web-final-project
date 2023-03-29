6ad447ae6b4c309bec0e5db292769d0b7c9b60b4
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2023-03-29 11:14:39 -0600

    added basic packages

---
+++ src/commands/ping.ts
@@ -0,0 +1,7 @@
+export const __ping: InteractionHandlerPayloads.GuildChatInputCommand = {
+    name: "ping",
+    description: "Testing!",
+    async execute(interaction) {
+        interaction.reply("Hello there!");
+    },
+}
