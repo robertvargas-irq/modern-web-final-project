bc1edc7beb26bc9a8c3a1415acec12bbf8b495cf
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2023-04-15 19:04:15 -0600

    added user declare

--- src/util/BotClient/BotClient.ts
+++ src/util/BotClient/BotClient.ts
@@ -1,10 +1,11 @@
-import { Client, Collection, ClientOptions } from "discord.js";
+import { Client, Collection, ClientOptions, ClientUser } from "discord.js";
 
 export default class BotClient extends Client {
     commands: Collection<
         keyof InteractionHandlerPayloads.GuildChatInputCommand["name"],
         InteractionHandlerPayloads.GuildChatInputCommand
     > = new Collection();
+    declare user: ClientUser;
 
     constructor(clientOptions: ClientOptions) {
         super(clientOptions);
