89f7bf2597be0931053a44e89ba679a198c31093
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2023-04-15 19:15:07 -0600

    added ready

---
+++ src/events/ready.ts
@@ -0,0 +1,25 @@
+import registerClientCommands from "../config/commands_register.js";
+import BotClient from "../util/BotClient/BotClient.js";
+import { ActivityType } from "discord.js";
+
+export default {
+    name: "ready",
+    once: "true",
+    execute(client: BotClient) {
+        registerClientCommands(client)
+            .then(console.log)
+            .catch((e) => {
+                console.error("Unable to parse client commands.", e);
+            });
+
+        client.user.setPresence({
+            activities: [
+                {
+                    name: "ALPHA | Hi!",
+                    type: ActivityType.Listening,
+                },
+            ],
+            status: "online",
+        });
+    },
+};
