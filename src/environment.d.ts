ea80a184443360d0184ed008a1af8586b85fc3b8
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2023-04-15 19:04:09 -0600

    added documentation

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -1,9 +1,23 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            ATLAS_TOKEN: string;
+            /**
+             * Atlas database user username.
+             */
             ATLAS_USER: string;
+            /**
+             * Atlas database user password.
+             */
+            ATLAS_TOKEN: string;
+            /**
+             * Discord bot login token.
+             */
             DISCORD_TOKEN: string;
+            /**
+             * Comma-separated guild ids for
+             * commands to be registered to.
+             * @example "100031,100032,100033"
+             */
             GUILDS: string;
         }
     }
