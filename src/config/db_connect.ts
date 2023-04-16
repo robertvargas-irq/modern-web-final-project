9b56de48129b4b5b91ecd35ff58e12d19ac25151
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2023-04-15 18:55:55 -0600

    added db connect

---
+++ src/config/db_connect.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+mongoose.set("strictQuery", true);
+
+const URI = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_TOKEN}@bot-data.7iantuj.mongodb.net/?retryWrites=true&w=majority`;
+
+export default async () =>
+    mongoose
+        .connect(URI)
+        .then((c) => (console.log("Database connection successful!"), c))
+        .catch((e) => (console.error(e), null));
