b3e87a249d140659c6fed8b5e9b59456eff46a4e
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2023-04-15 19:15:32 -0600

    Added client login

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import "dotenv/config";
 import db_connect from "./config/db_connect.js";
+import BotClient from "./util/BotClient/BotClient.js";
+import { GatewayIntentBits as GIB, Partials } from "discord.js";
 
 // connect to the database
 await db_connect();
 
+// create client and login
+const client = new BotClient({
+    intents: [GIB.Guilds, GIB.GuildMembers, GIB.GuildMessages],
+    partials: [Partials.Channel]
+});
+await client.login(process.env.DISCORD_TOKEN);
