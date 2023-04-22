ae666755cb1d5aa4617ff615bb81c9b6a083e9ea
bobbleheadhobo <supersoup4@gmail.com>
2023-04-21 21:33:04 -0600

    made it prettier

--- src/commands/bing.ts
+++ src/commands/bing.ts
@@ -1,10 +1,12 @@
 const __bing: InteractionHandlerPayloads.GuildChatInputCommand = {
     name: "bing",
-    description: "replies with bong",
+    description: "replies with bing-bong meme",
 
     async execute(interaction) {
-        interaction.reply("Bong!!!")
-    }
+        interaction.reply(
+            "https://tenor.com/view/sidetalk-sidetalknyc-bing-bong-bing-bong-coney-island-coney-island-gif-24203600"
+        );
+    },
 };
 
-export default __bing
+export default __bing;
