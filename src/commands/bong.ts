97c3f01cb735771cf7e14211c995f7563b1ef882
amarq30 <andresmrqz30@gmail.com>
2023-04-21 21:55:24 -0600

    Used prettier

--- src/commands/bong.ts
+++ src/commands/bong.ts
@@ -1,7 +1,7 @@
-const __bong: InteractionHandlerPayloads.GuildChatInputCommand ={
-    name:"bong",
-    description:"Replies with bong!",
-    async execute(interaction){
+const __bong: InteractionHandlerPayloads.GuildChatInputCommand = {
+    name: "bong",
+    description: "Replies with bong!",
+    async execute(interaction) {
         interaction.reply("Bing!");
     },
 };
