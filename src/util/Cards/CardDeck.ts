b07791970e2cd86e2b9b7ea67fd6aaeb67bc48c8
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2023-04-30 20:05:42 -0600

    updated

--- src/util/Cards/CardDeck.ts
+++ src/util/Cards/CardDeck.ts
@@ -1,11 +1,31 @@
-export class CardDeck {
+/**
+ * In-play faces.
+ * - 4 faces total.
+ */
+const DefaultFaces = 0b1111;
+/**
+ * In-play cards.
+ * Ace -> 2-10 -> Jack -> Queen -> King
+ */
+const Cards = 13;
 
-    private cards;
+export class CardDeck {
+    private cards: Array<number>;
+    private cardsRemaining: number;
 
     /**
      * Create a new CardDeck instance.
+     * @param decks The number of card decks
+     *              mixed into the current hand.
      */
-    constructor() {
+    constructor(decks: number = 1) {
+        // populate cards
+        this.cards = new Array(Cards);
+        for (let card = 0; card < Cards; card++) {
+            this.cards[card] = DefaultFaces;
+        }
 
+        // cache remaining cards
+        this.cardsRemaining = this.cards.length * 4;
     }
 }
