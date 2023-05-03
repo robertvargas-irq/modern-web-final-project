055fd323eaf00a7fe3bbb1910b8854e59198f89d
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2023-05-03 11:02:26 -0600

    added interaction reference

--- src/util/GameManager/GameManager.ts
+++ src/util/GameManager/GameManager.ts
@@ -3,6 +3,7 @@ import PlayerManager from "../Player/PlayerManager.js";
 import Dealer from "./Dealer.js";
 
 export default class GameManager {
+    private interaction: GuildInteractions.ChatInput;
     private players: PlayerManager;
     private dealer: Dealer;
     private deck: CardDeck;
@@ -12,6 +13,7 @@ export default class GameManager {
      * @param interaction The interaction that created the game.
      */
     constructor(interaction: GuildInteractions.ChatInput) {
+        this.interaction = interaction;
         this.players = new PlayerManager();
         this.dealer = new Dealer();
         this.deck = new CardDeck();
