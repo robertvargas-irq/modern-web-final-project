418e582f27b413048f796084cd0bc3d5500135bc
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2023-05-03 10:55:45 -0600

    added starter GameManager

---
+++ src/util/GameManager/GameManager.ts
@@ -0,0 +1,19 @@
+import CardDeck from "../Cards/CardDeck.js";
+import PlayerManager from "../Player/PlayerManager.js";
+import Dealer from "./Dealer.js";
+
+export default class GameManager {
+    private players: PlayerManager;
+    private dealer: Dealer;
+    private deck: CardDeck;
+
+    /**
+     * Create a new instance of GameManager.
+     * @param interaction The interaction that created the game.
+     */
+    constructor(interaction: GuildInteractions.ChatInput) {
+        this.players = new PlayerManager();
+        this.dealer = new Dealer();
+        this.deck = new CardDeck();
+    }
+}
