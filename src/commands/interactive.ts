a3910295a93781357de156c340f9402dacce56c8
amarq30 <andresmrqz30@gmail.com>
2023-05-04 15:29:48 -0600

    Mostly finished up interactive menu.

--- src/commands/interactive.ts
+++ src/commands/interactive.ts
@@ -1,12 +1,19 @@
+import { fetchMember } from "../util/MemberUtil/MemberFetch.js";
 import { Player } from "../util/Player/Player.js";
 import PlayerMenu from "../util/PlayerMenu/PlayerMenu.js";
-
 
 const __interactive: InteractionHandlerPayloads.GuildChatInputCommand = {
     name: "interactive",
     description: "An interactive menu to start!",
     async execute(interaction) {
-        const menu = new PlayerMenu());
+        const menu = new PlayerMenu(
+            interaction,
+            new Player(
+                await fetchMember(interaction.guildId, interaction.user.id),
+                interaction.member
+            )
+        );
+        menu.render();
     },
 };
 
--- src/util/PlayerMenu/PlayerMenu.ts
+++ src/util/PlayerMenu/PlayerMenu.ts
@@ -11,7 +11,9 @@ import {
 
 import { fetchUser } from "../UserUtil/UserFetch.js";
 import { Player } from "../Player/Player.js";
-
+/**
+ * Buttons constant just creates the current buttons for hit and stay.
+ */
 const buttons = new ActionRowBuilder<ButtonBuilder>().addComponents(
     new ButtonBuilder()
         .setCustomId("stay")
@@ -23,21 +25,40 @@ const buttons = new ActionRowBuilder<ButtonBuilder>().addComponents(
         .setStyle(ButtonStyle.Success)
 );
 
+/**
+ * Wrapper for Player Menus
+ */
 export default class PlayerMenu {
     private message?: Message;
     private interaction: RepliableInteraction;
     private player: Player;
+    private testForceStay: boolean;
 
+    /**
+     * Creates a new PlayerMenu
+     * @param interaction This is the interaction from the command to be able to reply
+     * @param player This is a player object to be able to access stuff within player.
+     */
     constructor(interaction: RepliableInteraction, player: Player) {
         this.interaction = interaction;
         this.player = player;
+        this.testForceStay = false;
     }
 
+    /**
+     * This will create embeds that will be displayed on the user side
+     * @returns embeds for generateMessagePayload
+     */
     private generateEmbeds() {
+        //The intial embed
         const embed = new EmbedBuilder()
             .setTitle("Welcome to BlackJack bot")
             .setDescription(
-                `Welcome ${this.player.member.displayName} to your BlackJack game!`
+                `Welcome ${
+                    this.player.member.displayName
+                } to your BlackJack game!\n
+                 You will force stay <t:${Math.floor(Date.now() / 1000 + 10)}:R>
+                `
             )
             .setColor(0x32cd32)
             .setThumbnail(this.interaction.user.displayAvatarURL())
@@ -58,16 +79,48 @@ export default class PlayerMenu {
                 },
             ]);
 
+        //If the player stays, we are going to remove the buttons and Give them a message to wait patiently
+        if (this.player.stay) {
+            embed
+                .setFields([])
+                .setImage(
+                    "https://media.discordapp.net/attachments/1090471775768428627/1099093963991961630/8IBKHtg0E484QKeXMPx4vyxD1czPK_ZzFtTQlMxm_c8.jpg?width=407&height=543"
+                )
+                .setDescription(
+                    `You have chosen to stay! \n\nWaiting for other players now! \n\nGood luck ${this.player.member.displayName}`
+                );
+        }
+
+        if (this.testForceStay) {
+            embed
+                .setFields([])
+                .setDescription(
+                    `You have been forced to stay with the hand you had because you took too long to choose. \n\nGood luck ${this.player.member.displayName}`
+                )
+                .setImage(
+                    "https://media.discordapp.net/attachments/1090471775768428627/1099093963991961630/8IBKHtg0E484QKeXMPx4vyxD1czPK_ZzFtTQlMxm_c8.jpg?width=407&height=543"
+                );
+        }
+
         return [embed];
     }
 
-    private generateMessagePayload() {
+    /**
+     * This will create the message to display to the play
+     * @param hideButtons This is to konw if we are hiding the buttons, only will happen if player stays
+     * @returns The object to display with the reply.
+     */
+    private generateMessagePayload(hideButtons: boolean) {
         return {
             embeds: this.generateEmbeds(),
-            components: [buttons],
+            components: hideButtons ? [] : [buttons],
+            ephemeral: true,
         };
     }
 
+    /**
+     * This function will intiialize the collector and reply depending on the buttons pressed.
+     */
     private initCollector() {
         if (!this.message) {
             throw new Error(
@@ -75,38 +128,58 @@ export default class PlayerMenu {
             );
         }
 
+        //initializer ofcollector
         const collector = this.message.createMessageComponentCollector({
             filter: (i) => i.user.id === this.interaction.user.id,
             componentType: ComponentType.Button,
-            time: 1000 * 10,
+            time: 10_000,
         });
 
+        //The responses once buttons are pressed
         collector.on("collect", (i) => {
+            i.deferUpdate();
+
             if (i.customId === "stay") {
-                i.deferUpdate;
+                this.player.stay = true;
+                console.log("We stayed in ur mom");
+                this.render(true);
                 return;
+            } else {
+                console.log("We hit your mom");
             }
-            return;
+
+            this.render();
         });
 
+        //At this point the timer has run out for the buttons
         collector.on("end", (collected) => {
+            if (collector.endReason === "time") {
+                this.player.stay = true;
+                this.testForceStay = true;
+                this.render(true);
+                console.log("You timed out!");
+            }
             console.log(`Collected ${collected.size} interactions`);
         });
     }
 
-    async render() {
+    /**
+     * This function will do all the rendering of the messages
+     * @param hideButtons Checks for hiding buttons, off by default since we start with all buttons available
+     * @returns Once the interaction has not be replied to
+     */
+    async render(hideButtons: boolean = false) {
         //Intitial Render
         if (!this.interaction.replied) {
             this.generateEmbeds();
             this.message = await this.interaction.reply({
-                ...this.generateMessagePayload(),
+                ...this.generateMessagePayload(hideButtons),
                 fetchReply: true,
             });
+            this.initCollector();
             return;
         }
 
-        this.interaction.editReply(this.generateMessagePayload());
+        this.interaction.editReply(this.generateMessagePayload(hideButtons));
     }
-
-    rerender() {}
 }
