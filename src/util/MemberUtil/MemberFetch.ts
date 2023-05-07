d94752cc45a4004449e2a52a3151f657804f2890
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2023-05-07 17:36:20 -0600

    added type

--- src/util/MemberUtil/MemberFetch.ts
+++ src/util/MemberUtil/MemberFetch.ts
@@ -1,5 +1,8 @@
-import MemberModel from "../../models/MemberModel.js";
+import MemberModel, { MemberDocument } from "../../models/MemberModel.js";
 
-export const fetchMember = async (guildId: string, userId: string) =>
+export const fetchMember = async (
+    guildId: string,
+    userId: string
+): Promise<MemberDocument> =>
     (await MemberModel.findOne({ guildId, userId })) ??
     new MemberModel({ guildId, userId });
