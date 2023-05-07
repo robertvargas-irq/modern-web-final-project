ea25aedca247749cec8f64bd45a7586915e1fa6b
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2023-05-06 18:37:34 -0600

    state is now determined by enumerable

--- src/util/Player/Player.ts
+++ src/util/Player/Player.ts
@@ -1,6 +1,8 @@
-import { GuildMember } from "discord.js";
+import { ButtonInteraction, GuildMember } from "discord.js";
 import { MemberDocument } from "../../models/MemberModel.js";
 import CardHand from "../Cards/CardHand.js";
+
+type PlayerState = "playing" | "loss" | "stay" | "force-stay";
 
 /**
  * Wrapper for players in-game.
@@ -8,9 +10,8 @@ import CardHand from "../Cards/CardHand.js";
 export class Player {
     public memberDoc: MemberDocument;
     public member: GuildMember;
+    public state: PlayerState;
     public cards: CardHand;
-    public loss: boolean;
-    public stay: boolean;
 
     /**
      * Creates a new Player instance.
@@ -20,17 +21,30 @@ export class Player {
     constructor(memberDoc: MemberDocument, member: GuildMember) {
         this.memberDoc = memberDoc;
         this.member = member;
-        this.loss = false;
-        this.stay = false;
+        this.state = "playing";
         this.cards = new CardHand();
+    }
+
+    /**
+     * Check if the player is currently staying.
+     * - True if the current player state is "stay" or "force-stay".
+     */
+    get staying() {
+        return this.state === "stay" || this.state === "force-stay";
     }
 
     /**
      * Resets stay, loss and cards for player
      */
     reset() {
-        this.stay = false;
-        this.loss = false;
+        this.state = "playing";
         this.cards.clear();
     }
+
+    /**
+     * Open a player's PlayerMenu on a given interaction.
+     * -
+     * @param interaction Interaction that prompted the menu open.
+     */
+    openPlayerMenu(interaction: ButtonInteraction) {}
 }
