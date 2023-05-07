56ebef4186cf252707db9fd923769f77b6738c00
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2023-05-06 23:39:18 -0600

    added proper handling of card value

--- src/util/Cards/CardHand.ts
+++ src/util/Cards/CardHand.ts
@@ -4,9 +4,34 @@ import { CardData } from "./CardData.js";
  * Wrapper for a player's hand of cards.
  */
 export default class CardHand {
-    cards: Array<CardData>;
+    private cards: Array<CardData>;
+    private totalValue: number;
+    private acesPresent: number;
     constructor() {
         this.cards = [];
+        this.totalValue = 0;
+        this.acesPresent = 0;
+    }
+
+    /**
+     * Check if the hand is empty.
+     */
+    get empty() {
+        return this.cards.length <= 0;
+    }
+
+    /**
+     * Get the total value of the current hand.
+     */
+    get value() {
+        return this.totalValue;
+    }
+
+    /**
+     * Check if the player has busted their hand.
+     */
+    get bust() {
+        return this.totalValue > 21;
     }
 
     /**
@@ -15,6 +40,21 @@ export default class CardHand {
      */
     add(card: CardData) {
         this.cards.push(card);
+        this.totalValue += Math.min(10, card.rank + 1);
+
+        // increment the number of aces present
+        if (card.rank === 0) {
+            this.acesPresent++;
+        }
+
+        // if a bust, check if salvagable with an ace
+        if (this.bust) {
+            while (this.acesPresent > 0 && this.bust) {
+                // convert the ace from 11 to 1
+                this.totalValue -= 10;
+                this.acesPresent--;
+            }
+        }
     }
 
     /**
